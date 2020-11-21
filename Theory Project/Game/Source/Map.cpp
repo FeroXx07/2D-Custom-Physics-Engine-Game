2950da3e8b6828adcfae37dbce5a49d7dbffd163
FeroXx07 <ali.shahid.1212@gmail.com>
2020-11-21 17:48:43 +0100

    Pre-setup

---
+++ Theory Project/Game/Source/Map.cpp
@@ -0,0 +1,321 @@
+
+#include "App.h"
+#include "Render.h"
+#include "Textures.h"
+#include "Map.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+#include <math.h>
+
+Map::Map() : Module(), mapLoaded(false)
+{
+    name.Create("map");
+}
+
+// Destructor
+Map::~Map()
+{}
+
+// L06: DONE 7: Ask for the value of a custom property
+int Properties::GetProperty(const char* value, int defaultValue) const
+{
+	ListItem<Property*>* item = list.start;
+
+	while (item)
+	{
+		if (item->data->name == value)
+			return item->data->value;
+		item = item->next;
+	}
+
+	return defaultValue;
+}
+
+// Called before render is available
+bool Map::Awake(pugi::xml_node& config)
+{
+    LOG("Loading Map Parser");
+    bool ret = true;
+
+    folder.Create(config.child("folder").child_value());
+
+    return ret;
+}
+
+// Draw the map (all requried layers)
+void Map::Draw()
+{
+	if (mapLoaded == false) return;
+
+	// L06: TODO 4: Make sure we draw all the layers and not just the first one
+	MapLayer* layer = data.layers.start->data;
+
+	// L04: DONE 5: Prepare the loop to draw all tilesets + DrawTexture()
+	for (int y = 0; y < data.height; ++y)
+	{
+		for (int x = 0; x < data.width; ++x)
+		{
+			int tileId = layer->Get(x, y);
+			if (tileId > 0)
+			{
+				// L04: TODO 9: Complete the draw function
+			}
+		}
+	}
+}
+
+// L04: DONE 8: Create a method that translates x,y coordinates from map positions to world positions
+iPoint Map::MapToWorld(int x, int y) const
+{
+	iPoint ret;
+
+	// L05: DONE 1: Add isometric map to world coordinates
+	if (data.type == MAPTYPE_ORTHOGONAL)
+	{
+		ret.x = x * data.tileWidth;
+		ret.y = y * data.tileHeight;
+	}
+	else if (data.type == MAPTYPE_ISOMETRIC)
+	{
+		ret.x = (x - y) * (data.tileWidth / 2);
+		ret.y = (x + y) * (data.tileHeight / 2);
+	}
+	else
+	{
+		LOG("Unknown map type");
+		ret.x = x; ret.y = y;
+	}
+
+	return ret;
+}
+
+// L05: DONE 2: Add orthographic world to map coordinates
+iPoint Map::WorldToMap(int x, int y) const
+{
+	iPoint ret(0, 0);
+
+	// L05: DONE 3: Add the case for isometric maps to WorldToMap
+	if (data.type == MAPTYPE_ORTHOGONAL)
+	{
+		ret.x = x / data.tileWidth;
+		ret.y = y / data.tileHeight;
+	}
+	else if (data.type == MAPTYPE_ISOMETRIC)
+	{
+
+		float half_width = data.tileWidth * 0.5f;
+		float half_height = data.tileHeight * 0.5f;
+		ret.x = int((x / half_width + y / half_height) / 2);
+		ret.y = int((y / half_height - (x / half_width)) / 2);
+	}
+	else
+	{
+		LOG("Unknown map type");
+		ret.x = x; ret.y = y;
+	}
+
+	return ret;
+}
+
+// L06: DONE 3: Pick the right Tileset based on a tile id
+TileSet* Map::GetTilesetFromTileId(int id) const
+{
+	ListItem<TileSet*>* item = data.tilesets.start;
+	TileSet* set = item->data;
+
+	while (item)
+	{
+		if (id < item->data->firstgid)
+		{
+			set = item->prev->data;
+			break;
+		}
+		set = item->data;
+		item = item->next;
+	}
+
+	return set;
+}
+
+// Get relative Tile rectangle
+SDL_Rect TileSet::GetTileRect(int id) const
+{
+	SDL_Rect rect = { 0 };
+
+	// L04: DONE 7: Get relative Tile rectangle
+	int relativeId = id - firstgid;
+	rect.w = tileWidth;
+	rect.h = tileHeight;
+	rect.x = margin + ((rect.w + spacing) * (relativeId % numTilesWidth));
+	rect.y = margin + ((rect.h + spacing) * (relativeId / numTilesWidth));
+	
+	return rect;
+}
+
+// Called before quitting
+bool Map::CleanUp()
+{
+    LOG("Unloading map");
+
+    // L03: DONE 2: Make sure you clean up any memory allocated from tilesets/map
+    // Remove all tilesets
+	ListItem<TileSet*>* item;
+	item = data.tilesets.start;
+
+	while (item != NULL)
+	{
+		RELEASE(item->data);
+		item = item->next;
+	}
+	data.tilesets.clear();
+
+	// L04: DONE 2: clean up all layer data
+	// Remove all layers
+	ListItem<MapLayer*>* item2;
+	item2 = data.layers.start;
+
+	while (item2 != NULL)
+	{
+		RELEASE(item2->data);
+		item2 = item2->next;
+	}
+	data.layers.clear();
+
+	// Clean up the pugui tree
+	mapFile.reset();
+
+    return true;
+}
+
+// Load new map
+bool Map::Load(const char* filename)
+{
+    bool ret = true;
+    SString tmp("%s%s", folder.GetString(), filename);
+
+    pugi::xml_parse_result result = mapFile.load_file(tmp.GetString());
+
+    if(result == NULL)
+    {
+        LOG("Could not load map xml file %s. pugi error: %s", filename, result.description());
+        ret = false;
+    }
+
+	// Load general info
+    if(ret == true)
+    {
+        // L03: DONE 3: Create and call a private function to load and fill all your map data
+		ret = LoadMap();
+	}
+
+    // L03: DONE 4: Create and call a private function to load a tileset
+    // remember to support more any number of tilesets!
+	pugi::xml_node tileset;
+	for (tileset = mapFile.child("map").child("tileset"); tileset && ret; tileset = tileset.next_sibling("tileset"))
+	{
+		TileSet* set = new TileSet();
+
+		if (ret == true) ret = LoadTilesetDetails(tileset, set);
+
+		if (ret == true) ret = LoadTilesetImage(tileset, set);
+
+		data.tilesets.add(set);
+	}
+
+	// L04: DONE 4: Iterate all layers and load each of them
+	// Load layer info
+	pugi::xml_node layer;
+	for (layer = mapFile.child("map").child("layer"); layer && ret; layer = layer.next_sibling("layer"))
+	{
+		MapLayer* lay = new MapLayer();
+
+		ret = LoadLayer(layer, lay);
+
+		if (ret == true)
+			data.layers.add(lay);
+	}
+    
+    if(ret == true)
+    {
+        // L03: TODO 5: LOG all the data loaded iterate all tilesets and LOG everything
+
+
+		// L04: TODO 4: LOG the info for each loaded layer
+    }
+
+    mapLoaded = ret;
+
+    return ret;
+}
+
+// L03: TODO: Load map general properties
+bool Map::LoadMap()
+{
+	bool ret = true;
+	pugi::xml_node map = mapFile.child("map");
+
+	if (map == NULL)
+	{
+		LOG("Error parsing map xml file: Cannot find 'map' tag.");
+		ret = false;
+	}
+	else
+	{
+		// L03: TODO: Load map general properties
+		
+	}
+
+	return ret;
+}
+
+// L03: TODO: Load Tileset attributes
+bool Map::LoadTilesetDetails(pugi::xml_node& tileset_node, TileSet* set)
+{
+	bool ret = true;
+	
+	// L03: TODO: Load Tileset attributes
+
+	return ret;
+}
+
+// L03: TODO: Load Tileset image
+bool Map::LoadTilesetImage(pugi::xml_node& tileset_node, TileSet* set)
+{
+	bool ret = true;
+	pugi::xml_node image = tileset_node.child("image");
+
+	if (image == NULL)
+	{
+		LOG("Error parsing tileset xml file: Cannot find 'image' tag.");
+		ret = false;
+	}
+	else
+	{
+		// L03: TODO: Load Tileset image
+	}
+
+	return ret;
+}
+
+// L04: TODO 3: Create the definition for a function that loads a single layer
+bool Map::LoadLayer(pugi::xml_node& node, MapLayer* layer)
+{
+	bool ret = true;
+	
+	// L04: TODO 3: Load a single layer
+
+	return ret;
+}
+
+
+// L06: TODO 6: Load a group of properties from a node and fill a list with it
+bool Map::LoadProperties(pugi::xml_node& node, Properties& properties)
+{
+	bool ret = false;
+
+	//...
+	return ret;
+}
+
---
+++ Theory Project/Game/Source/Scene.cpp
@@ -0,0 +1,104 @@
+#include "App.h"
+#include "Input.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Render.h"
+#include "Window.h"
+#include "Scene.h"
+#include "Map.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+Scene::Scene() : Module()
+{
+	name.Create("scene");
+}
+
+// Destructor
+Scene::~Scene()
+{}
+
+// Called before render is available
+bool Scene::Awake()
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool Scene::Start()
+{
+	// L03: DONE: Load map
+	//app->map->Load("hello2.tmx");
+	app->map->Load("iso_walk.tmx");
+	
+	// Load music
+	app->audio->PlayMusic("Assets/audio/music/music_spy.ogg");
+
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::Update(float dt)
+{
+    // L02: DONE 3: Request Load / Save when pressing L/S
+	if(app->input->GetKey(SDL_SCANCODE_L) == KEY_DOWN)
+		app->LoadGameRequest();
+
+	if(app->input->GetKey(SDL_SCANCODE_S) == KEY_DOWN)
+		app->SaveGameRequest();
+
+    // L08: TODO 6: Make the camera movement independent of framerate
+	if(app->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		app->render->camera.y -= ceil(200.0f*dt);
+
+	if(app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		app->render->camera.y += ceil(200.0f * dt);
+
+	if(app->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		app->render->camera.x -= ceil(200.0f * dt);
+
+	if(app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		app->render->camera.x += ceil(200.0f * dt);
+
+	// Draw map
+	app->map->Draw();
+
+	// L03: DONE 7: Set the window title with map/tileset info
+	/*SString title("Map:%dx%d Tiles:%dx%d Tilesets:%d",
+				   app->map->data.width, app->map->data.height,
+				   app->map->data.tileWidth, app->map->data.tileHeight,
+				   app->map->data.tilesets.count());*/
+
+	//app->win->SetTitle(title.GetString());
+
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PostUpdate()
+{
+	bool ret = true;
+
+	if(app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	return ret;
+}
+
+// Called before quitting
+bool Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	return true;
+}
